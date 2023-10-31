bea3ce1e3bc1dfde84e892b89915778acb87a568
hyung-2 <nh_0221@naver.com>
2023-10-31 18:11:11 +0900

    깃허브주소

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-/**
- * @format
- */
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import HomeScreen from './screens/HomeScreen'
+import MoreUserItem from './components/MoreUserItem'
 
-AppRegistry.registerComponent(appName, () => App);
+const Stack = createNativeStackNavigator()
+
+function App(){
+
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={{headerShown: false}}
+      >
+        <Stack.Screen name='Home' component={HomeScreen}/>
+        <Stack.Screen name='MoreInfo' component={MoreUserItem}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const sytles = StyleSheet.create({
+  block: {
+    flex: 1
+  }
+})
+
+
+export default App
